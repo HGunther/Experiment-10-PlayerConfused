d22f9a7c4a472c1172ffe0aeb24d3103ecadb5b6
Hans Gunther <hnsgunther@gmail.com>
2022-04-05 13:13:47 -0600

    Added an enemy who moves randomly

---
+++ PlayerConfused/Assets/Scrpits/AIMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public float movementForce = 1000;
+    public float directionSwitchTimer = 3f;
+
+    private float lastDirectionSwitch = 0f;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= lastDirectionSwitch + directionSwitchTimer)
+        {
+            PickDirection();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(direction * movementForce);
+    }
+
+    void PickDirection()
+    {
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        lastDirectionSwitch = Time.time;
+    }
+}
