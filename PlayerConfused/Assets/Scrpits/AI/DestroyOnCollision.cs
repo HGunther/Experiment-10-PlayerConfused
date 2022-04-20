b8d18fc3e7ca4ad206d67798a8526447ac7a199b
hgunther <hnsgunther@gmail.com>
2022-04-20 01:11:30 -0600

    Updated win condition

--- PlayerConfused/Assets/Scrpits/AI/DestroyOnCollision.cs
+++ PlayerConfused/Assets/Scrpits/AI/DestroyOnCollision.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
+    GameState gamestate;
+
+    private void Start()
+    {
+        gamestate = FindObjectOfType<GameState>();
+
+        if (!gamestate)
+        {
+            Debug.LogError("DestroyOnCollision could not find gamestate");
+        }
+    }
+
     void OnCollisionEnter2D (Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMovement>())
@@ -12,6 +24,9 @@ public class DestroyOnCollision : MonoBehaviour
             GetComponentInChildren<SpriteRenderer>().enabled = false;
             gameObject.SetActive(false);
             Destroy(gameObject);
+
+            Debug.Assert(gamestate, "DestoryOnCollision needs gamestate but it is invalid");
+            gamestate.ScorePoint();
         }
     }
 }
--- PlayerConfused/Assets/Scrpits/GameState.cs
+++ PlayerConfused/Assets/Scrpits/GameState.cs
@@ -6,8 +6,17 @@ public class GameState : MonoBehaviour
 {
     public delegate void VisionChanged(bool visionState);
     public static event VisionChanged OnVisionChanged;
+    public int score = 0;
+    int wormCount;
 
     public bool vision = false;
+
+    public GameObject winscreen;
+
+    private void Start()
+    {
+        wormCount = FindObjectsOfType<EnemyAI>().Length;
+    }
 
     public void ToggleVision()
     {
@@ -24,5 +33,23 @@ public class GameState : MonoBehaviour
         {
             ToggleVision();
         }
+    }
+
+    public void ScorePoint()
+    {
+        score += 1;
+        wormCount -= 1;
+        if (wormCount <= 0)
+        {
+            WinGame();
+        }
+    }
+
+    void WinGame()
+    {
+        Debug.Log("Win Game called");
+        Debug.Assert(winscreen, "No win screen found!");
+        winscreen.SetActive(true);
+        FindObjectOfType<GameState>().SetVision(true);
     }
 }
